f51be4a968dff643ec3de6b8055c1f51b2dbe557
mvbassalobre <bassalobre.vinicius@gmail.com>
2023-01-24 22:50:44 -0300

    redirect click logo

--- components/auth/AuthTemplate.tsx
+++ components/auth/AuthTemplate.tsx
@@ -4,6 +4,7 @@ import { Container } from "@/styles/global";
 import Image from "next/image";
 import styled from "styled-components";
 import { makeTitle } from "@/pages/index";
+import Link from "next/link";
 
 interface IProps {
   title: string;
@@ -23,12 +24,14 @@ export default function AuthTemplate(props: IProps) {
       <main>
         <Container>
           <TopBar>
-            <Image
-              src="/logo-light.svg"
-              alt="Picture of the author"
-              width={48}
-              height={48}
-            />
+            <Link href="/auth/sign-in">
+              <Image
+                src="/logo-light.svg"
+                alt="Picture of the author"
+                width={48}
+                height={48}
+              />
+            </Link>
           </TopBar>
           <Row alignX={"center"} alignY={"center"}>
             {props.children}
