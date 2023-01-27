03786fc0993bdff2db32eafc6f37b2186b0393b2
mvbassalobre <bassalobre.vinicius@gmail.com>
2023-01-27 15:29:01 -0300

    host on env

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -6,8 +6,8 @@ const handler = async (request: NextRequest): Promise<middlewareResponseInterfac
     const failed = () => ({ success: false, redirect: "/auth/sign-in", message: "Unauthorized !!", statusCode: 403 });
     try {
         const token = request.cookies.get("jwtToken")?.value ?? "";
-        const completeUrl = request.headers.get('referer');
-        const host = completeUrl?.split("/").slice(0, 3).join("/") ?? "";
+        const host = process.env.HOST;
+        console.log(`${host}/api/auth/check-token`)
         const response = await Http("POST", `${host}/api/auth/check-token`, { token });
 
         if (response.success) {
